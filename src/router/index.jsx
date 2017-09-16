dd68ff7b740bf4d32ac910a94fef397d0248d0df
MaoScut <849345372@qq.com>
2017-09-16 08:10:14 +0800

    只渲染到view级别的组件，再由view级别的组件去渲染component级别的组件

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Router, Route } from 'react-router-dom';
 // import Detail from '../components/Detail';
-import Editor from '../components/Editor';
-import { Home, Header, Error, PrivateArticles, Login, Detail } from '../components/Connected';
-import RequireAuth from '../components/RequireAuth';
+// import Editor from '../components/Editor';
+// import { Home, Header, Error, PrivateArticles, Login, Detail } from '../components/Connected';
+// import { Header } from '../components/Connected';
+import { Home, Detail, Header, Private } from '../views/Connected';
+// import RequireAuth from '../components/RequireAuth';
 import history from '../history';
 
 export default () => (
   <Router history={history}>
     <div>
       <Route path="/" component={Header} />
-      <Route path="/" component={Error} />
+      {/* <Route path="/" component={Error} /> */}
       <Route exact path="/" component={Home} />
       <Route exact path="/index.html" component={Home} />
-      <Route exact path="/login" component={Login} />
+      {/* <Route exact path="/login" component={Login} /> */}
       <Route path="/detail/:articleId" component={Detail} />
-      <Route path="/editor/:articleId" component={Editor} />
-      <Route exact path="/private" component={RequireAuth(PrivateArticles)} />
+      <Route path="/private" component={Private} />
+      {/* <Route path="/editor/:articleId" component={Editor} /> */}
+      {/* <Route exact path="/private" component={RequireAuth(PrivateArticles)} /> */}
     </div>
   </Router>
 );
