f70138661b3b223ab0dd6fa7777ccadf31dba2a9
MaoScut <849345372@qq.com>
2017-09-16 21:24:32 +0800

    添加每一层楼的样式

--- src/components/Floor/index.jsx
+++ src/components/Floor/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import './main.scss';
+import { timeDistance } from '../../utils';
 
-export default function Floor({ floor }) {
+export default function Floor({ item }) {
   return (
-    <li>
-      <span>{floor.userName}</span>
-      <span className="date">{floor.date}</span>
-      <p>{floor.content}</p>
-    </li>
+    <div className="floor">
+      <h2>{item.title}</h2>
+      <section>
+        <img className="user-head-img" src="/images/90.png" alt="" />
+        <div>
+          <p>用户名<span>{timeDistance(item.date)}</span></p>
+          <p>{item.content}</p>
+        </div>
+        <aside>
+          <i className="iconfont icon-aixin" />
+          <i className="iconfont icon-lianjie" />
+          <i className="iconfont icon-gengduo" />
+          <span>reply</span>
+        </aside>
+      </section>
+    </div>
   );
 }
