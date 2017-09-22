679e82e2624ce7382e46c5581cad0b35b31d2579
MaoScut <849345372@qq.com>
2017-09-22 12:27:02 +0800

    可无视的修改

--- server/data/utils.js
+++ server/data/utils.js
@@ -18,7 +18,52 @@ function writeAll(arr, p) {
     });
   });
 }
+
+// 连接两个表
+function linkTable(mainTable, assistantTable, mainProperty, assisProperty) {
+  // 返回obj的arr
+  return arr;
+}
+// 检查某个obj是否具有指定的属性
+// obj：要检查的对象
+// props：数组，元素是该obj需要具有的属性
+// 返回结果
+// 只要是具有这些属性就返回true，（多了属性也返回true）
+// 缺少任何一个就抛出异常
+function checkProperty(obj, props) {
+  const lackProps = [];
+  props.forEach((prop) => {
+    if (obj[prop] === undefined) lackProps.push(prop);
+  });
+  if (lackProps.length === 0) return true;
+  const errorMessage = `缺少属性：${lackProps.join(',')}`;
+  throw new Error(errorMessage);
+}
+
+function checkArrayElementProperty(arr, props) {
+  return arr.every(obj => checkProperty(obj, props));
+}
+
+function cutObj(obj, props) {
+  const newObj = {};
+  props.forEach((prop) => {
+    newObj[prop] = obj[prop];
+  });
+  return newObj;
+}
+
+function createError(message, type) {
+  return {
+    message,
+    type,
+  };
+}
+
 module.exports = {
   writeAll,
   readAll,
+  checkProperty,
+  checkArrayElementProperty,
+  createError,
+  cutObj,
 };
