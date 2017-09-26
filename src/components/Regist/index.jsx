fc6754ae16987fd4fdd7d3e1bd6e4e2d8e2d217d
MaoScut <849345372@qq.com>
2017-09-26 21:26:19 +0800

    添加用户名的输入验证，这样考虑起来，好像给出错误提示不是很简单啊。

--- src/components/Regist/index.jsx
+++ src/components/Regist/index.jsx
@@ -13,6 +13,12 @@ export default class Regist extends React.Component {
     this.saveEmail = this.saveEmail.bind(this);
     this.savePassword = this.savePassword.bind(this);
     this.saveUserName = this.saveUserName.bind(this);
+    this.checkAndSubmit = this.checkAndSubmit.bind(this);
+    this.ready = {
+      name: false,
+      email: false,
+      password: false,
+    };
   }
   componentWillReceiveProps(nextProps) {
     this.setState({
@@ -30,9 +36,24 @@ export default class Regist extends React.Component {
     });
   }
   saveUserName() {
-    this.setState({
-      userName: this.userNameInput.value,
-    });
+    if (/^[a-zA-Z]/.test(this.userNameInput.value)) {
+      this.ready.name = true;
+      this.setState({
+        userName: this.userNameInput.value,
+      });
+    } else {
+      this.ready.name = false;
+      this.userNameInput.value = '';
+    }
+  }
+  checkAndSubmit() {
+    if (this.ready.name && this.ready.email && this.ready.password) {
+      this.props.onSubmit({
+        email: this.state.email,
+        password: this.state.password,
+        userName: this.state.userName,
+      });
+    }
   }
   render() {
     return (
@@ -71,12 +92,7 @@ export default class Regist extends React.Component {
             <span>密码:&nbsp;</span>
           </div>
           <div className="btn-container">
-            <button onClick={() => this.props.onSubmit({
-              email: this.state.email,
-              password: this.state.password,
-              userName: this.state.userName,
-            })}
-            >注册</button>
+            <button onClick={this.checkAndSubmit}>注册</button>
             <button onClick={() => this.props.onCancel()}>取消</button>
           </div>
         </div>
