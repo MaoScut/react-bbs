7168f3ceab092fad9a81d2e977e7da235c873ba7
MaoScut <849345372@qq.com>
2017-09-29 10:11:26 +0800

    err有可能为null，要判断后才能访问属性

--- src/components/Regist/index.jsx
+++ src/components/Regist/index.jsx
@@ -8,7 +8,7 @@ export default class Regist extends React.Component {
       email: '',
       password: '',
       userName: '',
-      err: this.props.err,
+      err: this.props.err ? this.props.err.message : null,
     };
     this.saveEmail = this.saveEmail.bind(this);
     this.savePassword = this.savePassword.bind(this);
@@ -22,28 +22,52 @@ export default class Regist extends React.Component {
   }
   componentWillReceiveProps(nextProps) {
     this.setState({
-      err: nextProps.err,
+      err: nextProps.err ? nextProps.err.message : null,
     });
   }
   saveEmail() {
-    this.setState({
-      email: this.emailInput.value,
-    });
+    const reg = /[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/;
+    if (reg.test(this.emailInput.value)) {
+      this.ready.email = true;
+      this.setState({
+        email: this.emailInput.value,
+        err: null,
+      });
+    } else {
+      this.ready.email = false;
+      this.setState({
+        email: this.emailInput.value,
+        err: '邮箱格式错误！',
+      });
+    }
   }
   savePassword() {
-    this.setState({
-      password: this.passwordInput.value,
-    });
+    if (this.passwordInput.value !== '') {
+      this.ready.password = true;
+      this.setState({
+        password: this.passwordInput.value,
+        err: null,
+      });
+    } else {
+      this.ready.password = false;
+      this.setState({
+        err: '请输入密码',
+      });
+    }
   }
   saveUserName() {
     if (/^[a-zA-Z]/.test(this.userNameInput.value)) {
       this.ready.name = true;
       this.setState({
         userName: this.userNameInput.value,
+        err: null,
       });
     } else {
       this.ready.name = false;
-      this.userNameInput.value = '';
+      this.setState({
+        userName: this.userNameInput.value,
+        err: '用户名要以字母开头',
+      });
     }
   }
   checkAndSubmit() {
@@ -60,7 +84,7 @@ export default class Regist extends React.Component {
       <div className="login">
         <div className="head"><span>注册</span></div>
         <div className="input-container">
-          {this.props.err ? (<div><span>{this.props.err.message}</span></div>) : null}
+          <div><span>{this.state.err}</span></div>
           <div>
             <input
               type="text"
